c90c0ee87d7902fd0101d5ba6f999f2b3bbd94e5
Axyss <55812692+Axyss@users.noreply.github.com>
2020-05-27 10:40:07 +0200

    Update bot.py

--- AutomatiK files/bot.py
+++ AutomatiK files/bot.py
@@ -154,7 +154,7 @@ async def on_ready():
 
     """Start of the version checker"""
 
-    obj = updates.Check_Updates(local_version="v1.1_7",
+    obj = updates.Check_Updates(local_version="v1.1_8",
                                 link="https://github.com/Axyss/AutomatiK/releases")
     threading.Thread(target=obj.start_checking).start()  # Starts thread that checks updates
 
