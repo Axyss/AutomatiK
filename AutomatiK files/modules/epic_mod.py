93fb33d65741b9a54ea8a0337c07024992f0e1ab
Axyss <55812692+Axyss@users.noreply.github.com>
2020-08-26 14:32:02 +0200

    Add files via upload

--- AutomatiK files/modules/epic_mod.py
+++ AutomatiK files/modules/epic_mod.py
@@ -1,49 +1,40 @@
-import requests
-import sqlite3
-import time
 import json
 
+import requests
 
-class Scraping:
+from core.generic_mod import GenericModule
+from core.log_manager import logger
+
+
+class Epic(GenericModule):
 
     def __init__(self):
 
-        self.baseUrl = "https://www.epicgames.com/store/es-ES/product/"
-        self.gameData = []  # Data from free games (name, link)
-        self.validGameData = []  # Verified games from the DB method
-
-        self.endpoint = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=es-ES&country" \
-                        "=ES&allowCountries=ES "
-
-        self.data = None
-
-    def reset_request(self):
-
-        self.gameData = []
-        self.data = None
-        self.validGameData = []
+        self.SERVICE_NAME = "Epic Games"
+        self.MODULE_ID = "epic"
+        self.AUTHOR = "Default"
+        self.URL = "https://www.epicgames.com/store/es-ES/product/"
+        self.ENDPOINT = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=es-ES&country" \
+                        "=ES&allowCountries=ES"
 
     def make_request(self):
         """Makes the request and removes the unnecessary JSON data"""
 
-        self.reset_request()
-
         try:
-            self.data = requests.get(self.endpoint)
-
-            self.data = json.loads(self.data.content)  # Bytes to json object
+            raw_data = requests.get(self.ENDPOINT)
+            raw_data = json.loads(raw_data.content)  # Bytes to json object
+            raw_data = raw_data["data"]["Catalog"]["searchStore"]["elements"]  # Cleans the data
+            return raw_data
 
         except:
-            print(time.strftime('[%Y/%m/%d]' + '[%H:%M]') +
-                  "[ERROR]: Epic Games request failed!")
+            logger.error(f"Request to {self.SERVICE_NAME} failed!")
 
-        # Removes the not relevant information from the JSON object
-        self.data = self.data["data"]["Catalog"]["searchStore"]["elements"]
-
-    def process_request(self):  # Filters games that are not free
+    def process_request(self, raw_data):  # Filters games that are free
         """Returns the useful information form the request"""
 
-        for i in self.data:
+        processed_data = []
+
+        for i in raw_data:
             # i["promotions"]["upcomingPromotionalOffers"]
             try:
                 if (i["price"]["totalPrice"]["discountPrice"] == i["price"]["totalPrice"]["originalPrice"]) != 0:
@@ -51,66 +42,13 @@ class Scraping:
             except TypeError:
                 continue
 
-            # Parses relevant data such as name and link and adds It to gameData
-            temp = (i["title"], str(self.baseUrl + i["productSlug"]))
+            # Parses relevant data such as name and link and adds It to parsed_data
+            game = (i["title"], str(self.URL + i["productSlug"]))
+            processed_data.append(game)
+        return processed_data
 
-            self.gameData.append(temp)
+    def get_free_games(self):
 
-    def check_database(self):
-        """Manages the DB"""
-
-        # Opens DB connection.
-        conex = sqlite3.connect("db")
-        pointer = conex.cursor()
-
-        try:
-            # Tries to create the table if doesn't exist
-            try:
-                pointer.execute(
-                    "CREATE TABLE TABLA_1 (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE VARCHAR(50), LINK VARCHAR(100))"
-                )  # This line will create the table If It didn't already exist
-                conex.commit()
-
-            except sqlite3.OperationalError:  # If that table name already exists.
-                pass
-
-            cache = pointer.execute("SELECT NOMBRE FROM TABLA_1 WHERE ID > (SELECT MAX(ID) - 5 FROM TABLA_1)")
-            cache = cache.fetchall()
-            cache2 = []  # Here the information from the query is cleaned.
-
-            for i in cache:  # Tuples are removed here
-                cache2.append(i[0])
-
-            # If there is no cache or the game is already in the DB then skip...
-            try:
-
-                for i in self.gameData:
-
-                    if i[0] not in cache2:  # If param 0 does not match any of the DB then It's a new game.
-
-                        self.validGameData.append(i)  # Adds the data to a new list
-
-            except IndexError:
-                print(time.strftime('[%Y/%m/%d]' + '[%H:%M]') +
-                      "[ERROR]: Empty database")
-
-            if not self.validGameData:  # If validGameData empty return
-                return None
-
-            # Adds the register of the game to the DB
-            pointer.executemany("INSERT INTO TABLA_1 VALUES (NULL,?,?)", self.validGameData)  # Iterates the
-
-            print(time.strftime('[%Y/%m/%d]' + '[%H:%M]') +
-                  "[DB]: New register added:",
-                  self.validGameData
-                  )
-
-            return True  # If there games not registered in the database
-
-        # Closes connection under any circumstances
-        finally:
-            conex.commit()  # Saves changes
-            conex.close()
-
-
-obj = Scraping()
+        processed_data = self.process_request(self.make_request())
+        free_games = self.check_database(table="EPIC_TABLE", processed_data=processed_data)
+        return free_games
--- AutomatiK files/modules/humble_mod.py
+++ AutomatiK files/modules/humble_mod.py
@@ -1,106 +1,45 @@
-import requests
-import sqlite3
 import time
 
+import requests
 
-class Scraping:
+from core.log_manager import logger
+from core.generic_mod import GenericModule
+
+
+class Humble(GenericModule):
 
     def __init__(self):
 
-        self.source = "https://www.humblebundle.com/store/api/search?sort=discount&filter=onsale&request=1"
-        self.baseUrl = "https://www.humblebundle.com/store/"
-        self.gameData = []  # Data from free games (name, link)
-        self.validGameData = []  # Verified games from the DB method
-
-        self.data = None
-
-    def reset_request(self):
-
-        self.gameData = []
-        self.data = None
-        self.validGameData = []
+        self.SERVICE_NAME = "Humble Bundle"
+        self.MODULE_ID = "humble"
+        self.AUTHOR = "Default"
+        self.ENDPOINT = "https://www.humblebundle.com/store/api/search?sort=discount&filter=onsale&request=1"
+        self.URL = "https://www.humblebundle.com/store/"
 
     def make_request(self):
         """Makes the request and removes the unnecessary JSON data"""
 
-        self.reset_request()
-
         try:
-            self.data = requests.get(self.source).json()
-
+            raw_data = requests.get(self.ENDPOINT).json()
+            raw_data = raw_data["results"]
+            return raw_data
         except:
-            print(time.strftime('[%Y/%m/%d]' + '[%H:%M]') +
-                  "[ERROR]: Humble Bundle request failed!")
+            logger.error(f"Request to {self.SERVICE_NAME} failed!")
 
-        self.data = self.data["results"]
+    def process_request(self, raw_data):  # Filters games that are not free
+        """Returns the useful information from the request in a tuple"""
 
-    def process_request(self):  # Filters games that are not free
-        """Returns the useful information form the request"""
-
-        for i in self.data:
-
+        processed_data = []
+        for i in raw_data:
             if i["current_price"]["amount"] == 0:  # If game's price is 0
-
                 # Parses relevant data such as name and link and adds It to gameData
-                temp = (i["human_name"], str(self.baseUrl + i["human_url"]))
+                game = (i["human_name"], str(self.URL + i["human_url"]))
+                processed_data.append(game)
 
-                self.gameData.append(temp)
+        return processed_data
 
-    def check_database(self):
-        """Manages the DB"""
+    def get_free_games(self):
 
-        # Opens DB connection.
-        conex = sqlite3.connect("db")
-        pointer = conex.cursor()
-
-        try:
-            # Tries to create the table if doesn't exist
-            try:
-                pointer.execute(
-                    "CREATE TABLE TABLA_2 (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE VARCHAR(50), LINK VARCHAR(100))"
-                )  # This line will create the table If It didn't already exist
-                conex.commit()
-
-            except sqlite3.OperationalError:  # If that table name already exists.
-                pass
-
-            cache = pointer.execute("SELECT NOMBRE FROM TABLA_2 WHERE ID > (SELECT MAX(ID) - 8 FROM TABLA_2)")
-            cache = cache.fetchall()
-            cache2 = []  # Here the information from the query is cleaned.
-
-            for i in cache:  # Tuples are removed here
-                cache2.append(i[0])
-
-            # If there is no cache or the game is already in the DB then skip...
-            try:
-
-                for i in self.gameData:
-
-                    if i[0] not in cache2:  # If param 0 does not match any of the DB then It's a new game.
-
-                        self.validGameData.append(i)  # Adds the data to a new list
-
-            except IndexError:
-                print(time.strftime('[%Y/%m/%d]' + '[%H:%M]') +
-                      "[ERROR]: Empty database")
-
-            if not self.validGameData:  # If validGameData empty return
-                return None
-
-            # Adds the register of the game to the DB
-            pointer.executemany("INSERT INTO TABLA_2 VALUES (NULL,?,?)", self.validGameData)  # Iterates the
-
-            print(time.strftime('[%Y/%m/%d]' + '[%H:%M]') +
-                  "[DB]: New register added:",
-                  self.validGameData
-                  )
-
-            return True  # If there games not registered in the database
-
-        # Closes connection under any circumstances
-        finally:
-            conex.commit()  # Saves changes
-            conex.close()
-
-
-obj = Scraping()
+        processed_data = self.process_request(self.make_request())
+        free_games = self.check_database(table="HUMBLE_TABLE", processed_data=processed_data)
+        return free_games
