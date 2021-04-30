0c23d447daf7f1686c2c492c209e2035f1630684
Alejandro Nieto <alexnieto201@gmail.com>
2021-05-01 01:19:46 +0200

    Configuration file now contains database login credentials

--- AutomatiK files/core/config_manager.py
+++ AutomatiK files/core/config_manager.py
@@ -1,57 +1,65 @@
 import json
 import os
-
 from core.log_manager import logger
 
 
 class ConfigManager:
+    def __init__(self, filename):
+        self.CONFIG_TEMPLATE = {"MONGODB": {
+                                    "HOST": "127.0.0.1",
+                                    "PORT": "27017",
+                                    "USER": "",
+                                    "PASSWORD": "",
+                                    "AUTH_SOURCE": "",
+                                    "AUTH_MECHANISM": "DEFAULT"
+                                    }
+                                }
+        self.FILENAME = filename
+        self.config = None
+        self._create_config()
+        self.load_config()
 
-    def __init__(self, config_name):
-        self.config_name = config_name
-        self.data_config = None  # Loaded config
-
-        # Template of the config
-        self.data_template = {"mentioned_role": "<@&1234>",
-                              "lang": "en_EN"}
-
-    def create_config_keys(self, modules):
-        """Creates fields in the configuration file for new modules."""
-        for i in modules:
-            if i.MODULE_ID not in list(self.data_config.keys()):
-                self.edit_config(f"{i.MODULE_ID}_status", True)
-
-    def check_config_changes(self):
-        """Adds new fields to the configuration If the template has more."""
-        template_keys = list(self.data_template.keys())
-        current_keys = list(self.data_config.keys())
-
-        for i in template_keys:  # Checks the differences between configs
-            if i in current_keys:
-                continue
-            else:
-                self.edit_config(i, self.data_template[i])  # Adds the element using the value from the template
-                logger.debug(f"New field detected in the configuration template. "
-                             f"Adding '{i}': '{self.data_template[i]}'")
+    def _create_config(self):
+        """Creates the configuration file and dumps in it the configuration template if the file does not exist."""
+        try:
+            open(self.FILENAME, "x").close()
+            with open(self.FILENAME, "w") as f:
+                json.dump(self.CONFIG_TEMPLATE, f, indent=2)
+        except FileExistsError:
+            logger.debug("Configuration file already exists, omitting...")
 
     def load_config(self):
-        """Loads the config from the file."""
-        if self.config_name in os.listdir("."):  # If config file already exists
+        """Loads into memory the information of the configuration file."""
+        with open(self.FILENAME, "r") as f:
+            config = json.load(f)
+            self.config = config
 
-            with open(self.config_name, "r") as file:
-                self.data_config = json.load(file)  # Loads config
-                file.close()
+    @staticmethod
+    def _parse_var(value):
+        """
+           Obtains the value from the JSON configuration file, directly, or from an environment variable if
+           the syntax is: var('env_var').
+        """
+        if value.startswith("env('") and value.endswith("')"):
+            env_var = value[value.find("'") + 1: value.rfind("'")]
+            return os.getenv(env_var)
+        else:
+            return value
 
-        else:  # Creates the config file and writes the template into It
-            with open(self.config_name, "w") as file:
-                json.dump(self.data_template, file, indent=2)  # Injects the template
-                file.close()
-                self.data_config = self.data_template  # Loads the template into the current config
-        logger.info("Configuration loaded")
+    def get_mongo_host(self):
+        return ConfigManager._parse_var(self.config["MONGODB"]["HOST"])
 
-    def edit_config(self, key, value):
-        """Provides a handy way to modify both configs (permanent and loaded)."""
-        self.data_config[key] = value  # Alters loaded config
-        file = open(self.config_name, "w")
-        json.dump(self.data_config, file, indent=2)  # Rewrites the permanent config using the loaded
-        file.close()
-        logger.debug(f"Configuration key '{key}' was successfully assigned the value '{value}'")
+    def get_mongo_port(self):
+        return ConfigManager._parse_var(self.config["MONGODB"]["PORT"])
+
+    def get_mongo_user(self):
+        return ConfigManager._parse_var(self.config["MONGODB"]["USER"])
+
+    def get_mongo_pwd(self):
+        return ConfigManager._parse_var(self.config["MONGODB"]["PASSWORD"])
+
+    def get_mongo_auth_source(self):
+        return ConfigManager._parse_var(self.config["MONGODB"]["AUTH_SOURCE"])
+
+    def get_mongo_auth_mechanism(self):
+        return ConfigManager._parse_var(self.config["MONGODB"]["AUTH_MECHANISM"])
