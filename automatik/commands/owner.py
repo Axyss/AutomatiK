fa88a262cd91ff0b9cbf8193ee389cc060e74d4f
Axyss <contact@axyss.dev>
2026-04-10 14:43:37 +0200

    removed: owner commands /start and /stop

--- automatik/commands/owner.py
+++ automatik/commands/owner.py
@@ -15,39 +15,6 @@ class OwnerSlash(commands.Cog):
         return await self.bot.is_invoked(interaction)
 
     @app_commands.command()
-    async def start(self, interaction):
-        """Starts the main service globally."""
-        guild_lang = self.database.get_guild_config(interaction.guild)["lang"]
-
-        if self.bot.main_loop:
-            await interaction.response.send_message(self.languages.get_message(guild_lang, "start_already"))
-            return None
-
-        self.bot.main_loop = True
-        await interaction.response.send_message(self.languages.get_message(guild_lang, "start_success"))
-        logger.info(
-            f"Main loop started by {interaction.user} ({interaction.user.id}) "
-            f"from guild '{interaction.guild.name}' ({interaction.guild.id})"
-        )
-
-    @app_commands.command()
-    @app_commands.checks.has_permissions(administrator=True)
-    async def stop(self, interaction):
-        """Stops the main service globally."""
-        guild_lang = self.database.get_guild_config(interaction.guild)["lang"]
-
-        if not self.bot.main_loop:  # If service already stopped
-            await interaction.response.send_message(self.languages.get_message(guild_lang, "stop_already"))
-            return None
-
-        self.bot.main_loop = False
-        await interaction.response.send_message(self.languages.get_message(guild_lang, "stop_success"))
-        logger.info(
-            f"Main loop stopped by {interaction.user} ({interaction.user.id}) "
-            f"from guild '{interaction.guild.name}' ({interaction.guild.id})"
-        )
-
-    @app_commands.command()
     @app_commands.checks.has_permissions(administrator=True)
     async def reload(self, interaction):
         """Reloads configuration, services and language packages."""
